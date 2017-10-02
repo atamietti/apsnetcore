44f76c5381bbb1fa1f4fdfad97d6e5f718f02567
André Tamietti Quintão <andretamietti@hotmail.com>
2017-10-01 21:53:20 -0300

    m2

---
+++ Module2/mvc/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using mvc.Models;
+
+namespace mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            
+        }
+        public IActionResult Index()
+        {
+            ViewData["Environment"] =  _configuration.GetValue<string>("Environment");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
