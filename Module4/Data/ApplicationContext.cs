e5aaddd95902ec5ce431e2a6a88167f4405f7a4a
André Tamietti Quintão <andretamietti@hotmail.com>
2017-10-03 01:38:31 -0300

    entity

---
+++ Module4/Data/ApplicationContext.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Module4.Domain;
+
+namespace Module4.Data
+{
+    public class ApplicationContext: DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext>  options): base(options)
+        {
+
+        }
+        
+        public DbSet<Product>  Products {get;set;}
+        public DbSet<Category> Categories { get; set; }
+
+      protected  override void OnModelCreating(ModelBuilder modelBuilder) 
+      {
+
+            modelBuilder.Entity<Product>().ToTable("Prod");
+            modelBuilder.Entity<Product>().Property(p=> p.Name ).HasMaxLength(100);
+            modelBuilder.Entity<Category>().ToTable("Cat");
+            modelBuilder.Entity<Category>().Property(p=> p.Name ).HasMaxLength(50);
+        }
+    }
+}
---
+++ Module4/Domain/Category.cs
@@ -0,0 +1,10 @@
+namespace Module4.Domain
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
