3816406cd0f73206b225b668ccffa3152bd73e55
André Tamietti Quintão <andretamietti@hotmail.com>
2017-10-05 23:42:09 -0300

    teste

---
+++ Carrossel/Controllers/PedidoController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Carrossel.Controllers
+{
+    public class PedidoController : Controller
+    {
+        public IActionResult Carrossel()
+        {
+            return View();
+        }
+
+
+        public IActionResult Carrinho()
+        {
+        return View();
+        }
+
+        public IActionResult Resumo()
+        {
+        return View();
+        }
+    }
+}
