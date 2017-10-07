567c51b62b8a36bdf6a252fa47d6ed6f18fa3368
André Tamietti Quintão <andretamietti@hotmail.com>
2017-10-07 01:32:48 -0300

    Migrations

---
+++ module7/src/StoreOfBuild.DI/Bootsrap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using StoreOfBuild.Data;
+
+namespace StoreOfBuild.DI
+{
+    public class Bootstrap
+    {
+        public static void Configure(IServiceCollection services, string connectionString)
+        {
+
+                services.AddDbContext<ApplicationDbContext>(options=>
+                     options.UseSqlServer(connectionString));
+        }
+    }
+}
---
+++ module7/src/StoreOfBuild.Web/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StoreOfBuild.Web.Models;
+
+namespace StoreOfBuild.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateOrEdit()
+        {
+            // ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateOrEdit(int id)
+        {
+            // ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+    }
+}
